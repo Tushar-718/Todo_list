59eaa5ac80d22266bc9c5456ca228ee003382a6f
Tushar-718 <tushar718kumar@gmail.com>
2023-10-10 13:23:47 +0530

    crud operations

--- index.js
+++ index.js
@@ -1,27 +1,80 @@
-let form= document.getElementById("form");
-let textInput= document.getElementById("textInput");
-let warning= document.getElementById("warning");
-let dateInput= document.getElementById("dateInput");
-let textarea= document.getElementById("textarea");
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let warning = document.getElementById("warning");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
 
- form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
+    // to prevent refresh
     e.preventDefault();
     formValidation();
- });
+});
 
- let formValidation =() =>{
-    if(textInput.value === ""){
+let formValidation = () => {
+    if (textInput.value === "") {
         console.log("failure");
         warning.innerHTML = "Task cannot be blank";
-    }
-    else{
+    } else {
         console.log("Success");
         warning.innerHTML = "";
+        acceptData();
+        add.setAttribute("data-bs-dismiss","modal");
+        add.click();
+        (()=>{
+            add.setAttribute("data-bs-dismiss","");
+        })()
     }
- }
+}
 
- let data ={};
 
- let acceptData =() =>{
-    
- };
+// accept task from user 
+let data = {};
+
+let acceptData = () => {
+    data["text"] = textInput.value;
+    data["date"] = dateInput.value;
+    data["description"] = textarea.value;
+    createTasks();
+};
+
+
+// displaying task on browser
+let createTasks = () => {
+    // + is used so that old tasks get concatenated to new task
+    tasks.innerHTML += `            
+    <div>
+    <span class="fw-bold">${data.text}</span>
+    <span class="small text-secondary fw-bold">${data.date}</span>
+    <p>${data.description}</p>
+    <span class="options">
+        <button onClick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="e_button" type="button">Edit</button>
+        <button onClick="deleteTask(this)" class="d_button" type="button">Delete</button>
+    </span>
+    </div>`;
+    resetForm();
+};
+
+
+// reseting of form
+let resetForm=()=>{
+    textInput.value="";
+    dateInput.value="";
+    textarea.value="";
+}
+
+// deleting the task
+let deleteTask = (e)=>{
+    e.parentElement.parentElement.remove();
+}
+
+
+// updating the task
+let editTask =(e)=>{
+    let selectedTask = e.parentElement.parentElement;
+    textInput.value=selectedTask.children[0].innerHTML;
+    dateInput.value=selectedTask.children[1].innerHTML;
+    textarea.value=selectedTask.children[2].innerHTML;
+    selectedTask.remove();
+}
